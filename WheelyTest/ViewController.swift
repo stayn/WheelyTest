9874b7d3b459fd5a5a9e7bd45a80f99107471c1e
Stayn <nastyaloginovaa@gmail.com>
2017-04-10 12:52:23 +0300

    Initial Commit

---
+++ WheelyTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WheelyTest
+//
+//  Created by Anastasia Loginova on 10.04.17.
+//  Copyright © 2017 Anastasia Loginova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
